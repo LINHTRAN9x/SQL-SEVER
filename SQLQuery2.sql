ab0c8d74120d332d7d014381d5da93885e3bcd44
Trần Văn Linh <133183567+LINHTRAN9x@users.noreply.github.com>
2023-08-17 09:48:17 +0700

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,30 @@
+USE AdventureWorks2019;
+SELECT * from Customers
+
+SELECT CustomerID
+From dbo.Customers
+WHERE City = 'London'
+
+CREATE TYPE dbo.Testing
+FROM NVarchar(20) NULL;
+
+SELECT * FROM dbo.Testing;
+
+DECLARE @Number int;
+SET @Number = 2+2* (4 + (5-3));
+SELECT @Number
+
+SELECT * FROM Sales.SalesOrderHeader;
+SELECT SalesOrderID, CustomerID, SalesPersonID, TerritoryID, YEAR(OrderDate)
+       AS CurrentYear, YEAR(OrderDate) +1 AS NextYear
+	   FROM Sales.SalesOrderHeader
+GO --batch
+
+--comment likes this--and /* like this */
+SELECT SalesPersonID, YEAR(OrderDate) AS OrderYear
+FROM Sales.SalesOrderHeader
+WHERE CustomerID = 30084
+GROUP BY SalesPersonID, YEAR(OrderDate)
+HAVING COUNT(*) > 1
+ORDER BY SalesPersonID, YEAR(OrderDate)
+
